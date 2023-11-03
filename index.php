5831ced4114898746bd2ef57f97bda79e0a25c53
Natalia Jezusek <natalieonmars@gmail.com>
2023-11-03 20:36:58 +0100

    Started_working_on_the_layout

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moja Strona</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <h2 class ="title">
+            VISUAL HARMONY
+        </h2>
+        <div class="navbar">
+            <a href="#">STRONA GŁÓWNA</a>
+            <a href="#">KONTAKT</a>
+            <a href="#">LOGOWANIE</a>
+        </div>
+    </header>
+
+    <section>
+        <img class ="logo" src="visual_logo.png">
+    </section>
+
+    <section>  
+        <div class = "search">
+            <div>
+                <button class="magnifier">
+                    <img src="lupa.png">
+                </button>
+            </div>
+            <div>
+                <input class ="searchbar" type="text" placeholder="Szukaj po tagach">
+            </div>            
+        </div>
+        
+
+        
+    </section>
+
+
+    <footer>
+        <p>&copy; Copyright © 2023 Visual Harmony. Created by: Natalia Jezusek, Nikola Niestrój, Jacek Kozak, Daniel Łątkowski.</p>
+    </footer>
+</body>
+
+</html>
