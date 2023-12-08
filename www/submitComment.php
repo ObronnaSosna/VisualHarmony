8072724e5aad2ad319bdaf8d88b5f90f44006de7
DanielLatkowskiii <daniel.latkowski@student.po.edu.pl>
2023-12-08 19:21:29 +0100

    Finally comment section is working only style left

--- www/submitComment.php
+++ www/submitComment.php
@@ -9,23 +9,33 @@ if (!$conn) {
 $defaultUserId = 1;
 
 if (isset($_POST['postId']) && isset($_POST['text'])) {
-    $postId = $_POST['postId'];
+    $postId = (int)$_POST['postId']; // Zamiana na liczbę całkowitą
     $commentText = $_POST['text'];
 
     $query = "INSERT INTO comments (post_id, text, users_id) VALUES (?, ?, ?)";
     $stmt = mysqli_prepare($conn, $query);
 
     mysqli_stmt_bind_param($stmt, "isi", $postId, $commentText, $defaultUserId);
-    mysqli_stmt_execute($stmt);
 
-    if ($stmt) {
+    $result = mysqli_stmt_execute($stmt);
+
+
+    if ($result) {
         echo json_encode(['status' => 'success', 'message' => 'Comment saved successfully']);
     } else {
-        echo json_encode(['status' => 'error', 'message' => 'Error saving comment: ' . mysqli_error($conn)]);
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            // Jeśli to jest asynchroniczne żądanie AJAX, zwróć odpowiedź JSON
+            echo json_encode(['status' => 'error', 'message' => 'Error saving comment', 'errorDetails' => mysqli_error($conn)]);
+        } else {
+            // W przeciwnym razie zwróć pełną stronę HTML z błędem
+            die('Error saving comment: ' . mysqli_error($conn));
+        }
     }
+    
+
+    mysqli_close($conn);
 } else {
     echo json_encode(['status' => 'error', 'message' => 'postId or text parameter is missing.']);
 }
 
-mysqli_close($conn);
 ?>
