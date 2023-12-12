bfdee6971e72d04649343143b664014c908b80ba
Nikola <nikola.niestroj@gmail.com>
2023-12-12 19:45:30 +0100

    VH-Fixed navigation responsiveness issue

--- www/frame/topBar.php
+++ www/frame/topBar.php
@@ -3,8 +3,8 @@
         VISUAL HARMONY
     </h2>
     <div class="navbar">
-        <a href="index.php" title="Strona główna" class="a-menu">STRONA GŁÓWNA</a>
-        <a href="onas.php" title="Poznaj nas" class="a-menu">O NAS</a>
+        <a href="index.php" title="Strona główna" class="a-menu">STRONA<span class="invisible-space"></span>GŁÓWNA</a>
+        <a href="onas.php" title="Poznaj nas" class="a-menu">O<span class="invisible-space"></span>NAS</a>
         <a href="kontakt.php" title="Skontaktuj się z nami" class="a-menu">KONTAKT</a>
         <a href="logowanie.php" title="Przejdź do strony logowania" class="a-menu">LOGOWANIE</a>
     </div>
